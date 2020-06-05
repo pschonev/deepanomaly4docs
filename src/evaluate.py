de39dbec4516e169336234c617d8e23d955737aa
Philipp <philipp@omnius.com>
2020-06-05 22:56:29 +0200

    working eval

--- src/evaluate.py
+++ src/evaluate.py
@@ -26,7 +26,7 @@ df = pd.read_pickle(
     "/home/philipp/projects/dad4td/data/processed/20_news_imdb.pkl")
 
 # decrease dataset size
-fraction_of_data = 0.2
+fraction_of_data = 0.4
 df = df.sample(frac=fraction_of_data, replace=False, random_state=42)
 
 # text
@@ -38,31 +38,29 @@ y = (y * -1).clip(lower=0)
 y[y == 0] = -1
 
 
+X = TfidfVectorizer(stop_words='english', min_df=50).fit_transform(X)
+
 pipe = Pipeline([
     # the reduce_dim stage is populated by the param_grid
-    ('vectorize', 'passthrough'),
     ('reduce_dim', umap.UMAP(random_state=42)),
     ('classify', LocalOutlierFactor(novelty=True, contamination=0.183))
 ])
 
 
-MIN_DF = [50]
 MIX_RATIO = [0.1, 0.18]
 N_COMPONENTS = [20, 50, 100]
-METRICS = ['hellinger']
+METRICS = ['manhattan', 'cosine', 'euclidean', 'hellinger']
 param_grid = [
     {
-        'vectorize': [TfidfVectorizer(stop_words='english'), CountVectorizer(stop_words='english')],
-        'vectorize__min_df': MIN_DF,
         'reduce_dim__set_op_mix_ratio': MIX_RATIO,
         'reduce_dim__n_components': N_COMPONENTS,
         'reduce_dim__metric': METRICS
     }
 ]
 
-grid = GridSearchCV(pipe, scoring='f1', param_grid=param_grid, cv=3, verbose=10, n_jobs=-1)
+grid = GridSearchCV(pipe, scoring='f1', param_grid=param_grid, cv=5, verbose=10, n_jobs=-1)
 grid.fit(X, y)
 
 out_df = pd.DataFrame.from_dict(grid.cv_results_)
 out_df = out_df.sort_values(by=['rank_test_score'])
-out_df.to_csv("/home/philipp/projects/dad4td/reports/eval_4.tsv", sep="\t")
+out_df.to_csv("/home/philipp/projects/dad4td/reports/eval_5.tsv", sep="\t")
