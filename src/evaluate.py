5e5a8f34c33a3d0be4a61bccfd7f98932bfae2fc
Philipp <philipp@omnius.com>
2020-06-06 15:15:33 +0200

    eval 6 done

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,30 +3,18 @@ import umap
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pactools.grid_search import GridSearchCVProgressBar
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA, NMF
-from sklearn.decomposition import TruncatedSVD
-from sklearn.base import BaseEstimator, TransformerMixin
-
-class SparseToArray(BaseEstimator,TransformerMixin):
-
-    def transform(self, X):
-        # what other output you want
-        return X.toarray()
-
-    def fit(self, X, y=None, **fit_params):
-        return self
+from enstop import EnsembleTopics
 
 # %%
 df = pd.read_pickle(
     "/home/philipp/projects/dad4td/data/processed/20_news_imdb.pkl")
 
 # decrease dataset size
-fraction_of_data = 0.4
+fraction_of_data = 0.2
 df = df.sample(frac=fraction_of_data, replace=False, random_state=42)
 
 # text
@@ -37,24 +25,29 @@ y = df["outlier_label"]
 y = (y * -1).clip(lower=0)
 y[y == 0] = -1
 
-
-X = TfidfVectorizer(stop_words='english', min_df=50).fit_transform(X)
-
 pipe = Pipeline([
     # the reduce_dim stage is populated by the param_grid
-    ('reduce_dim', umap.UMAP(random_state=42)),
+    ('vectorize', TfidfVectorizer(stop_words='english')),
+    ('reduce_dim', 'passthrough'),
     ('classify', LocalOutlierFactor(novelty=True, contamination=0.183))
 ])
 
-
-MIX_RATIO = [0.1, 0.18]
-N_COMPONENTS = [20, 50, 100]
-METRICS = ['manhattan', 'cosine', 'euclidean', 'hellinger']
-param_grid = [
+MIN_DF=[5, 30, 50, 90]
+MIX_RATIO=[0.1, 0.18]
+N_COMPONENTS=[20, 50, 100]
+METRICS=['hellinger','manhattan', 'cosine', 'euclidean']
+param_grid=[
     {
-        'reduce_dim__set_op_mix_ratio': MIX_RATIO,
+        'vectorize__min_df': MIN_DF,
+        'reduce_dim': [umap.UMAP(random_state=42)],
         'reduce_dim__n_components': N_COMPONENTS,
+        'reduce_dim__set_op_mix_ratio': MIX_RATIO,
         'reduce_dim__metric': METRICS
+    },
+        {
+        'vectorize__min_df': MIN_DF,
+        'reduce_dim': [EnsembleTopics(random_state=42)],
+        'reduce_dim__n_components': N_COMPONENTS
     }
 ]
 
@@ -63,4 +56,4 @@ grid.fit(X, y)
 
 out_df = pd.DataFrame.from_dict(grid.cv_results_)
 out_df = out_df.sort_values(by=['rank_test_score'])
-out_df.to_csv("/home/philipp/projects/dad4td/reports/eval_5.tsv", sep="\t")
+out_df.to_csv("/home/philipp/projects/dad4td/reports/eval_6.tsv", sep="\t")
