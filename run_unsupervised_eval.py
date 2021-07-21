288cecf1e6d03c04b2b1fdb2bc14de951e80ed28
Philipp <philipp@omnous.com>
2021-07-21 15:18:46 +0200

    added support for hydra configs and changed dataclasses to pydantic

---
+++ run_unsupervised_eval.py
@@ -0,0 +1,13 @@
+from omegaconf import DictConfig
+from src.unsupervised_eval import eval_unsup
+import hydra
+
+
+
+@hydra.main(config_path="./configs", config_name="config")
+def eval_unsupervised(cfg: DictConfig) -> None:   
+    eval_run = hydra.utils.instantiate(cfg.experiment)
+    eval_unsup(eval_run)
+
+if __name__ == "__main__":   
+    eval_unsupervised()
